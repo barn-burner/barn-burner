d62660cf8fddc557ad92a0f59987d502dd707ed3
Ryan Long <rlong@redhat.com>
2022-05-24 13:53:12 -0400

    add matchup and games route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import Landing from './routes/Landing';
 import NotFound from './routes/NotFound';
+import Matchup from './routes/Matchup';
+import Game from './routes/Game';
 
 import './App.scss';
 
@@ -12,6 +14,8 @@ const App: React.FC = () => {
     <Fragment>
       <Switch>
         <Route exact path='/' component={ Landing }/>
+        <Route path='/matchup' component={ Matchup }/>
+        <Route path='/game' component={ Game }/>
         <Route component={ NotFound } />
       </Switch>
     </Fragment>
---
+++ frontend/src/routes/Matchup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Matchup.scss';
+
+const Matchup: React.FC = () => {
+
+  return (
+    <span> Matchup </span>
+  );
+};
+
+export default Matchup;
